8bb0a58c10f1baa5bf3530f75d3e588464557dbb
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-11-17 20:22:56 +0800

    feat:menambahkan navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome | IndoTreasure</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-</head>
+@section('container')
 
-<body>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container">
-            <a class="navbar-brand" href="/">
-                <img src="" alt="IndoTreasure" width="30" height="24">
-            </a>
-        </div>
-    </nav>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
-</body>
+<h1>This Is Home Page</h1>
 
-</html>
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar" style="background-color: #004080">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+        <a class="navbar-brand" href="/">
+            <img src="img/word.png" alt="Logo" width="250" class="d-inline-block align-text-top">
+        </a>
+        <div class="d-flex">
+            <button class="btn category-button">Kategori</button>
+            <div class="input-group has-validation ms-3">
+                <span class="input-group-text"><i class="bi bi-search"></i></span>
+                <div class="form-floating">
+                    <input type="text" class="form-control custom-search-input" id="floatingInputGroup2" placeholder="Pencarian..." required>
+                    <label for="floatingInputGroup2">Pencarian...</label>
+                </div>
+            </div>
+        </div>
+        <div class="buttons-container">
+            <a class="btn login-button">Login</a>
+            <a class="btn register-button">Register</a>
+        </div>
+    </div>
+</nav>
