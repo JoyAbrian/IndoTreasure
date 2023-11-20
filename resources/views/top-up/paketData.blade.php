13d0162527c8d9cdc76d583e82a3942cb3b31d14
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-11-20 21:43:29 +0800

    feat:tambah page paketData

--- resources/views/top-up/paketData.blade.php
+++ resources/views/top-up/paketData.blade.php
@@ -2,5 +2,14 @@
 
 @section('container')
 
+<h3>Beli Paket Data</h3>
+<div class="col-lg-6">
+    <div class="input-group mb-3">
+        <span class="input-group-text">Nomor Telepon</span>
+        <input type="tel" class="form-control" id="no-telp" name="no-telp" size="10" required>
+    </div>
+    <h4>Nominal</h4>
+    
+</div>
 
 @endsection
