3bc3010c0b3296d29dffb2e355632a06eb55fac3
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-11-29 01:06:24 +0800

    Fix: update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,28 +36,6 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('ZFED5u3QN9x7ykwzqA4s8W')
         ]);
 
-        User::create([
-            'name' => 'Khaeruozikkin',
-            'username' => '@erulzzw69',
-            'email' => 'Khaeruozikkin@yahoo.com',
-            'role' => 'admin',
-            'password' => bcrypt('JegelKom')
-        ]);
-
-        User::create([
-            'name' => 'Muh. Ilham Syafitra Hendra',
-            'username' => '@senpaiIlham',
-            'email' => 'senpaikunn@yahoo.com',
-            'role' => 'admin',
-            'password' => bcrypt('NitoKun1167')
-        ]);
-
-        User::create([
-            'name' => 'Andi Muh. Haikal Lukman',
-            'username' => '@dccal',
-            'email' => 'haikallukman@gmail.com',
-            'role' => 'admin',
-            'password' => bcrypt('dccalForever')
-        ]);
+        
     }
 }
