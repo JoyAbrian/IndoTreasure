e7dae1f32af01bd82cfe7ef6d54b1b4341c6e08a
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-11-30 11:07:20 +0800

    Fix:update beberapa

--- database/seeders/SellerSeeder.php
+++ database/seeders/SellerSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Seller;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SellerSeeder extends Seeder
 {
@@ -12,6 +13,31 @@ class SellerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Seller::create([
+            'name' => 'Fantech Official Store',
+            'slug' => 'fantechstore',
+            'bio' => 'Fantech Official Store Resmi Brand Fantech | Garansi Resmi Fantech www.fantech.id Customer Care WA : 081119094439',
+            'address' => 'Jakarta Barat',
+            'email' => 'marketingfantech@gmail.com',
+            'phone_number' => '081119094439'
+        ]);
+
+        Seller::create([
+            'name' => '',
+            'slug' => '',
+            'bio' => '',
+            'address' => '',
+            'email' => '',
+            'phone_number' => ''
+        ]);
+
+        Seller::create([
+            'name' => '',
+            'slug' => '',
+            'bio' => '',
+            'address' => '',
+            'email' => '',
+            'phone_number' => ''
+        ]);
     }
 }
