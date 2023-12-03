bdac0fea35d1c6f26e7e9d8b5f4c4cb3cfb29665
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-12-04 03:04:07 +0800

    fix: fix gender di factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,13 +20,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $gender = ['Male', 'Female'];
+
         return [
             'name' => fake()->name(),
             'birthdate' => fake()->date(),
             'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'primary_phone_number' => fake()->unique()->phoneNumber(),
-            'gender' => 'Male',
+            'gender' => fake()->randomElement($gender),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
